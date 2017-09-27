bd08f7b7ae5cc0ac8861af9b0afca51caf22629f
dianamcnulty <dianamcnulty@gmail.com>
2017-09-27 11:03:32 -0400

    Add logic for player toggle
    players now toggle between x and o.
    Cell array built and accepts player marks at appropriate index.

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,6 +1,15 @@
+let player = 'X'
+const cells = ['', '', '', '', '', '', '', '', '']
 const playerMark = function (event) {
   console.log('player took a turn', event.target.id)
-  $('#' + event.target.id).text('X')
+  $('#' + event.target.id).text(player)
+  cells[event.target.id] = player
+  console.log(cells)
+  if (player === 'X') {
+    player = 'O'
+  } else {
+    player = 'X'
+  }
 }
 module.exports = {
   playerMark
