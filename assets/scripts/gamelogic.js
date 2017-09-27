7797ff758902f708d8712185f62581d8f2a561a4
dianamcnulty <dianamcnulty@gmail.com>
2017-09-27 11:42:25 -0400

    Add logic for findWinner
    created new file for findWinner - this logic might be verbose!
    first winning combo working and tested.

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,18 +1,26 @@
+const findWinner = require('./findWinner')
 let player = 'X'
-const cells = ['', '', '', '', '', '', '', '', '']
+const cells = []
 const playerMark = function (event) {
-  //console.log('player took a turn', event.target.id)
-  $('#' + event.target.id).text(player)//updated the cell
-
+  // console.log('player took a turn', event.target.id)
+  $('#' + event.target.id).text(player)// updates the cell
   cells[event.target.id] = player
+
   console.log(cells)
+  const winner = findWinner.findWinner()
+  if (winner) {
+    console.log(winner)
+    return winner
+  }
   if (player === 'X') {
     player = 'O'
   } else {
     player = 'X'
   }
-  $('#player').text(player)
+  $('#player').text(player) // changes player name in the message box
 }
+
 module.exports = {
-  playerMark
+  playerMark,
+  cells
 }
