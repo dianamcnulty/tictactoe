ac17ecc0b22030552735c53adadba6174e4346e0
dianamcnulty <dianamcnulty@gmail.com>
2017-09-27 13:36:41 -0400

    Add UI updates when winner declared
    Hides game board and player move field when winner declared
    also announces specific winner.

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -2,75 +2,14 @@ const winlogic = require('./winlogic')
 const findWinner = winlogic.findWinner
 let player = 'X'
 const cells = []
-
-// const findWinner = function () {
-//   console.log(cells)
-//   // I know this is ugly and ridiculous! but I think it will work. This if statement checks if the winning combinations have the same player in those cells
-//   if (cells[0] && cells[1] && cells[2]) {
-//     if (cells[0] === cells[1] && cells[0] === cells[2]) {
-//       const player = cells[0]
-//       console.log('Winner is ', player)
-//       return player
-//     }
-//   }
-//   if (cells[3] && cells[4] && cells[5]) {
-//     if (cells[3] === cells[4] && cells[3] === cells[5]) {
-//       const player = cells[3]
-//       console.log('Winner is ', player)
-//       return player
-//     }
-//   }
-//   if (cells[6] && cells[7] && cells[8]) {
-//     if (cells[6] === cells[7] && cells[8] === cells[7]) {
-//       const player = cells[6]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-//   if (cells[0] && cells[3] && cells[6]) {
-//     if (cells[0] === cells[3] && cells[0] === cells[6]) {
-//       const player = cells[6]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-//   if (cells[1] && cells[4] && cells[7]) {
-//     if (cells[1] === cells[4] && cells[1] === cells[7]) {
-//       const player = cells[7]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-//   if (cells[2] && cells[5] && cells[8]) {
-//     if (cells[2] === cells[5] && cells[2] === cells[8]) {
-//       const player = cells[2]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-//   if (cells[0] && cells[4] && cells[8]) {
-//     if (cells[0] === cells[4] && cells[0] === cells[8]) {
-//       const player = cells[8]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-//   if (cells[6] && cells[4] && cells[2]) {
-//     if (cells[6] === cells[4] && cells[6] === cells[2]) {
-//       const player = cells[6]
-//       console.log('Winner is ' + player + '!!')
-//       return player
-//     }
-//   }
-// }
-
 const playerMark = function (event) {
   // console.log('player took a turn', event.target.id)
   $('#' + event.target.id).text(player)// updates the cell
   cells[event.target.id] = player
   const winner = findWinner(cells)
-  if (winner) {
-    console.log(winner)
+  if (winner !== undefined) {
+    console.log('winner is', winner)
+    declareWinner(winner)
     return winner
   }
   if (player === 'X') {
@@ -79,6 +18,12 @@ const playerMark = function (event) {
     player = 'X'
   }
   $('#player').text(player) // changes player name in the message box
+}
+
+const declareWinner = function (winner) {
+  $('#winnername').text(winner)
+  $('.board').hide()
+  $('#next-move').hide()
 }
 module.exports = {
   playerMark,
--- assets/scripts/winlogic.js
+++ assets/scripts/winlogic.js
@@ -6,56 +6,48 @@ const findWinner = function (cells) {
   if (cells[0] && cells[1] && cells[2]) {
     if (cells[0] === cells[1] && cells[0] === cells[2]) {
       const player = cells[0]
-      console.log('Winner is ', player)
       return player
     }
   }
   if (cells[3] && cells[4] && cells[5]) {
     if (cells[3] === cells[4] && cells[3] === cells[5]) {
       const player = cells[3]
-      console.log('Winner is ', player)
       return player
     }
   }
   if (cells[6] && cells[7] && cells[8]) {
     if (cells[6] === cells[7] && cells[8] === cells[7]) {
       const player = cells[6]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
   if (cells[0] && cells[3] && cells[6]) {
     if (cells[0] === cells[3] && cells[0] === cells[6]) {
       const player = cells[6]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
   if (cells[1] && cells[4] && cells[7]) {
     if (cells[1] === cells[4] && cells[1] === cells[7]) {
       const player = cells[7]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
   if (cells[2] && cells[5] && cells[8]) {
     if (cells[2] === cells[5] && cells[2] === cells[8]) {
       const player = cells[2]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
   if (cells[0] && cells[4] && cells[8]) {
     if (cells[0] === cells[4] && cells[0] === cells[8]) {
       const player = cells[8]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
   if (cells[6] && cells[4] && cells[2]) {
     if (cells[6] === cells[4] && cells[6] === cells[2]) {
       const player = cells[6]
-      console.log('Winner is ' + player + '!!')
       return player
     }
   }
