1859bbc507645c50e383a7b53dd0d36860eac09b
dianamcnulty <dianamcnulty@gmail.com>
2017-09-27 11:10:30 -0400

    Player message update

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,8 +1,9 @@
 let player = 'X'
 const cells = ['', '', '', '', '', '', '', '', '']
 const playerMark = function (event) {
-  console.log('player took a turn', event.target.id)
-  $('#' + event.target.id).text(player)
+  //console.log('player took a turn', event.target.id)
+  $('#' + event.target.id).text(player)//updated the cell
+
   cells[event.target.id] = player
   console.log(cells)
   if (player === 'X') {
@@ -10,6 +11,7 @@ const playerMark = function (event) {
   } else {
     player = 'X'
   }
+  $('#player').text(player)
 }
 module.exports = {
   playerMark
