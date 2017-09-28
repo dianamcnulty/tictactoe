15e0789fbf4278bf8965d67e02b0268ea2056f20
dianamcnulty <dianamcnulty@gmail.com>
2017-09-28 15:00:04 -0400

    Fix sign-up issue
    Fixed issue with opening game on sign-up.
    now requires users to log in to get their token.

--- assets/scripts/authui.js
+++ assets/scripts/authui.js
@@ -2,31 +2,32 @@ const gameui = require('./gameui')
 const store = require('./store')
 
 const signUpSuccess = function (data) {
-  console.log('signed up successfully', store)
-  gameui.showGame()
+  // console.log('signed up successfully', store)
+  $('#signupmessage').text('Thanks for signing up! Enter your new user name and password to log in.')
+  $('.error').hide()
+  $('#sign-up').hide()
 }
-const authFail = function (error) {
-  console.log('sign up error', error)
+const authFail = function () {
   $('.error').show()
 }
 const logInSuccess = function (data) {
   store.user = data.user
-  console.log('logged in successfully', store)
+  // console.log('logged in successfully', store)
   gameui.showGame()
+  $('.error').hide()
 }
 const logOutSuccess = function (data) {
   store.user = null
-  console.log('logged out successfully')
+  // console.log('logged out successfully')
   gameui.hideGame()
   $('#auth').show()
 }
 const passwordSuccess = function (data) {
-  console.log('password changed successfully')
+  // console.log('password changed successfully')
   $('#password-complete').text('Your password has beeen updated. Please use your new password next time you log in.')
   $('#password-update').hide()
 }
-const passwordFail = function (error) {
-  console.log('password error', error)
+const passwordFail = function () {
   $('#password-complete').text('uh oh... your password update didn\'t process. Please Try again')
 }
 module.exports = {
